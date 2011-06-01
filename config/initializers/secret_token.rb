5b9cc8ff0830d0d260ac1327737b90a7742d27dd
Sara Gibbons <sarag@vacationview.com>
2011-05-31 21:51:43 -0400

    initial commit of project using spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Combatgent::Application.config.secret_token = '42c114113937ced3e73e2e227ff26685d2b5dff715978a505a002352aa953e47a8eed4a2dc983106bbcdb3689942a77a096f08a4b8dd97f03286847b9593c14d'
