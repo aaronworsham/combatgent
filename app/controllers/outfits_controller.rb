495b725d79f1a2d62c62201ba939ec15583558fe
Sara Gibbons <sarag@vacationview.com>
2011-12-28 14:37:56 -0500

    fix outfit page

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -2,8 +2,8 @@ class OutfitsController < Spree::BaseController
   respond_to :html
   
   def show
-    @product = Product.find(params[:id])
-    @outift = @product.first
+    @product = Product.find(params[:product_id])
+    @outfit = Outfit.find(params[:id])
   end
 
 end
