bdacea62e0800bdc23c6f9029157afaac321fdfe
Nate Luzod <nate.luzod@mac.com>
2012-01-08 20:23:43 -1000

    adding products, changing products per page

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -10,11 +10,20 @@ module SpreeSite
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      
+      Spree::Config.set(:products_per_page => 20)      
     end
     
     def load_tasks
     end
     
     config.to_prepare &method(:activate).to_proc
+    
+    
+       
   end
+  
+
+  
 end
+
