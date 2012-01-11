6f8599abe3785cb4e1fdc0410e9cdbc74adc9b38
Nate Luzod <nate.luzod@mac.com>
2012-01-10 18:49:25 -1000

    product detail page

--- public/javascripts/combat_gent.js
+++ public/javascripts/combat_gent.js
@@ -11,6 +11,14 @@ jQuery(document).ready(function(){
 	$("#order_use_billing").change(function(){
 		$("#shipping .inner").slideToggle();
 	});
+	
+	/* TO-DO: Moving price to the right spot, we should get this done in Ruby instead. */
+	var actualPrice = "<span>" + $(".price.selling").text() + "</span>";
+	$("p.prices").remove();
+	$("#product-title").append(actualPrice);
+	
+	// $("#add-to-closet-form").appendTo("#cart-form");
+	// $("#match-to-outfit").appendTo("#cart-form");	
 
 
 });
