4458c89f7ae8a3884029d5dd6ef978782f498250
Sara Gibbons <sarag@vacationview.com>
2012-01-15 20:29:43 -0500

    fix outfit image bucket for s3

--- app/models/outfit_image.rb
+++ app/models/outfit_image.rb
@@ -3,7 +3,7 @@ class OutfitImage < Image
   has_attached_file :attachment,
                     :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
                     :storage => :s3,
-                    :bucket => 'combatgentlesirs',
+                    :bucket => 'combatgent',
                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                     :default_style => :small,
                     :url => "outfits/:id/:style/:basename.:extension",
