db2b613b0f4e86904f9d59c9bee52cc9a1ee2905
Nate Luzod <nate.luzod@mac.com>
2012-02-27 01:01:19 -1000

    reverting billing change

--- public/javascripts/combat_gent.js
+++ public/javascripts/combat_gent.js
@@ -8,16 +8,16 @@ jQuery(document).ready(function(){
 					// console.log(response.posts[0].title);
    });
 
-	if($("#order_use_billing").is(":checked")){
-		$("#shipping .inner").hide();
-		$(this).change(function(){
-			$("#shipping .inner").slideToggle();
-		});
-	} else {
-		$(this).change(function(){
-			$("#shipping .inner").slideToggle();
-		});		
-	}
+	// if($("#order_use_billing").is(":checked")){
+	// 	$("#shipping .inner").hide();
+	// 	$(this).change(function(){
+	// 		$("#shipping .inner").slideToggle();
+	// 	});
+	// } else {
+	// 	$(this).change(function(){
+	// 		$("#shipping .inner").slideToggle();
+	// 	});		
+	// }
 
 	// $("#order_use_billing").change(function(){
 	// 	$("#shipping .inner").slideToggle();
